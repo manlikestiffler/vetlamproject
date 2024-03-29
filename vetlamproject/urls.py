27fbb63dd9d81821a34a6fb05bef42d2925e20ff
manlikestiffler <tinashegomo96@gmail.com>
2024-03-29 15:27:01 +0200

    Update urls.py

--- vetlamproject/urls.py
+++ vetlamproject/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path('',include('ticket.urls', namespace='ticket')),
 ]
 
-urlpatterns += staticfiles_urlpatterns
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
